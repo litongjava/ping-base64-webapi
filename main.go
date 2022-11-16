99573384096286298729f524d963d891158a6e7e
litongjava <litongjava@qq.com>
2022-11-16 08:34:11 +0800

    add port args

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"ping-base64-webapi/controller"
 )
 
 func main() {
+	port := "10405"
+	for i := 1; i < len(os.Args); i += 2 {
+		param := os.Args[i]
+		if param == "--port" {
+			port = os.Args[i+1]
+		}
+	}
 	log.Println("start")
 	controller.RegisterRoutes()
-	err := http.ListenAndServe(":10404", nil)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		log.Println(err)
+		log.Fatalln(err)
 	}
 }
