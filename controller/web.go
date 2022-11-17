82391d00c448a5ca166b4ac3775013d6b152b4f0
litongjava <litongjava@qq.com>
2022-11-17 15:04:19 +0800

    对返回内容使用base64加密

--- controller/web.go
+++ controller/web.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"ping-base64-webapi/services"
@@ -20,10 +21,22 @@ func handleWeb(writer http.ResponseWriter, request *http.Request) {
 	if len(matches) > 0 {
 		bytes, _ := base64.StdEncoding.DecodeString(matches[1])
 		result := services.RunWrapperCommand(string(bytes))
-		encoder := json.NewEncoder(writer)
-		err := encoder.Encode(result)
+		//encoder := jsonString.NewEncoder(writer)
+		//err := encoder.Encode(result)
+		//对返回内容使用base64加密
+		jsonString, err := json.Marshal(result)
 		if err != nil {
-			log.Println("err", err)
+			log.Println("err", err.Error())
+			_, err = fmt.Fprintln(writer, err.Error())
+			if err != nil {
+				return
+			}
+			return
+		}
+		base64String := base64.StdEncoding.EncodeToString([]byte(jsonString))
+		_, err = fmt.Fprintln(writer, base64String)
+		if err != nil {
+			return
 		}
 	} else {
 		log.Println("not find base64 string")
